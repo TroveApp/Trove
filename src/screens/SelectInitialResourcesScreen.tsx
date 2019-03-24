563faa5ee2f7f8db0dc9b41e44a017805156b90f
Aaron Friel <aaron@twochairs.com>
2019-03-24 09:25:18 -0700

    Update resource IDs with backend IDs

--- src/screens/SelectInitialResourcesScreen.tsx
+++ src/screens/SelectInitialResourcesScreen.tsx
@@ -48,15 +48,15 @@ export class SelectInitialResourcesScreen extends React.Component<Props, State> 
             <Text style={styles.subtitleText}>Select all that apply</Text>
             <BubblePicker
               bubbles={[
-                {text: "Therapy", id: "therapy"},
-                {text: "Meditation", id: "meditation"},
-                {text: "Journaling", id: "journaling"},
-                {text: "Fitness", id: "fitness"},
-                {text: "Yoga", id: "yoga"},
-                {text: "Acupuncture", id: "acupuncture"},
-                {text: "Massage", id: "massage"},
-                {text: "Medication", id: "medication"},
-                {text: "Reading", id: "reading"},
+                {text: "Therapy", id: "43107345-d3e3-43c5-8782-cbcb7cd6dc7d"},
+                {text: "Meditation", id: "5073bee9-b689-41fe-9be1-e25b6eb2be79"},
+                {text: "Journaling", id: "c6f30162-1651-4aff-8ec9-f36c3453705f"},
+                {text: "Fitness", id: "eb46d033-e983-4226-bcde-e4783da21660"},
+                {text: "Yoga", id: "cc571cc2-488c-419e-9e4d-b05237f2b308"},
+                {text: "Acupuncture", id: "38bb19e1-0093-4156-9299-d3ae6cf6b90e"},
+                {text: "Massage", id: "57cc11e5-2572-48aa-aee1-00fc41b42d38"},
+                {text: "Medication", id: "3cba103d-3bf5-439a-b529-526479571b8a"},
+                {text: "Reading", id: "d3614d4b-ac2a-47fe-87aa-b5a968149170"},
               ]}
               selectedBubbles={this.state.selectedResources}
               onChangeSelectedBubbles={selectedBubbles => this.setState({selectedResources: selectedBubbles})}
